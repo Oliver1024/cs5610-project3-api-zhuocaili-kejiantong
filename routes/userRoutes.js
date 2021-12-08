43d68ab0496b37f8d6bed90c20ada5f1ea84a18e
Zhuocai Li <zhuocaili@Zhuocais-MacBook-Air.local>
2021-12-08 01:27:09 -0800

    api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -73,7 +73,7 @@ router.put('/profile', auth_middleware, (req, res) => {
 });
 
 router.get('/logout', auth_middleware, function (req, res) {
-    return res.clearCookie('onBoardToken').status(200).send({ message: 'Logout successfully!' });
+    return res.cookie('onBoardToken', { expires: Date.now() }).status(200).send({ message: 'Logout successfully!' });
 })
 
 router.get('/favorites', auth_middleware, (req, res) => {
